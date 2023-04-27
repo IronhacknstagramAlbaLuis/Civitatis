4c46cf751773b49457374b2b9800e91b429b2d0b
Luis Escribano <johndoe@example.com>
2023-04-27 19:19:31 +0200

    Navbar

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,11 @@
 
-import ActivitiesList from "./components/activities/activities-list/ActivitiesList";
+
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
-      <ActivitiesList/>
+      <Navbar />
     </>
   );
 }

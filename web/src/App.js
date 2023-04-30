8a71f8a0a100720b897b93041bfc5bc3660614a6
Luis Escribano <johndoe@example.com>
2023-04-30 11:26:43 +0200

    route

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ActivitiesList from "./components/activities/activities-list/ActivitiesList";
-import HomePage from "./pages/HomePage";
-
+import ActivitiesPage from "./pages/ActivitiesPage";
+import HomePage from "./pages/HomePage";import ActivitiesPage from "./pages/ActivitiesPage";
 
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/activities/:id " element={<ActivitiesPage/>} />
+      
+
       </Routes>
      
       <Footer />

a20e459a388839ac55e16f6b8050c06e8f301a47
Luis Escribano <johndoe@example.com>
2023-04-30 21:29:41 +0200

    token

--- web/src/App.js
+++ web/src/App.js
@@ -3,10 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ActivitiesList from "./components/activities/activities-list/ActivitiesList";
-
 import HomePage from "./pages/HomePage";
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import  AuthStore  from './components/context/AuthStore'
 
 
 
@@ -14,17 +14,17 @@ import LoginPage from './pages/LoginPage';
 function App() {
   return (
     <>
-      <Navbar />
-    
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path="/activities/:id " element={<ActivitiesPage/>} /> */}
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage/>} />
-
-      </Routes>
-     
-      <Footer />
+      <AuthStore>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/activities/:id " element={<ActivitiesPage/>} /> */}
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage/>} />
+          </Routes> 
+        
+        <Footer />
+      </AuthStore>
     </>
   );
 }
---
+++ web/src/components/context/AuthStore.js
@@ -0,0 +1,20 @@
+
+import React, { createContext, useState } from 'react'
+
+const AuthContext = createContext()
+
+function AuthStore({ children }) {
+  const [user, setUser] = useState()
+
+  const handleUserChange = (user) => {
+    console.log(user)
+     }
+  return (
+     <AuthContext.Provider value={{user, onUserChange: handleUserChange}}>
+      { children }
+     </AuthContext.Provider>
+  )
+}
+
+export {AuthStore as default, AuthContext} 
+

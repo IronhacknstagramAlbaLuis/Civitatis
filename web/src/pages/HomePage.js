19b64ff7b79fc26c0d2bf0fb5e2f2cbea490abb9
AlbaGremar <albagmarquez@gmail.com>
2023-05-05 20:01:40 +0200

    buscador

--- web/src/pages/HomePage.js
+++ web/src/pages/HomePage.js
@@ -15,27 +15,33 @@ function Home () {
    		  <div className="box search">
 			    <h3>Llena tu viaje</h3>
          	<h1>Excursiones en español por todo el mundo</h1>
-           <form className="d-flex" role="search">
-              <input className="form-control me-2 searchform" type="search" placeholder="Where are you traveling to?" aria-label="Search"/>
-              <button className="btn btn-outline-success enter" type="submit"><img src={searchicon} className='searchicon' alt="Go!"/></button>
-           </form>
+            <button className="btn"><span>¿Donde vas a viajar?</span><i class="material-icons">public</i>
+              <ul className="dropdown menuanim">
+                <li className="active"><Link to = {`/activities/filter?destiny=Roma`}><p>Roma</p></Link></li>
+                <li><Link to = {`/activities/filter?destiny=Bali`}><p>Bali</p></Link></li>
+                <li><Link to = {`/activities/filter?destiny=NuevaYork`}><p>Nueva York</p></Link></li>
+                <li><Link to = {`/activities/filter?destiny=Egipto`}><p>Egipto</p></Link></li>
+                <li><Link to = {`/activities/filter?destiny=Paris`}><p>París</p></Link></li>
+                <li><Link to = {`/activities/filter?destiny=Londres`}><p>Londres</p></Link></li>
+              </ul>
+            </button>
 		    </div>
 
    		  <div className="box tips">
 			    <div className="tip">
-				    <p>Las mejores actividades</p>
+            <p><span class="material-icons">celebration</span>Las mejores actividades</p>
 			    </div>
 
 			    <div className="tip">
-				    <p>Atención al cliente 24/7</p>
+				    <p><span class="material-icons">support_agent</span>Atención al cliente 24/7</p>
 			    </div>
 
 			    <div className="tip">
-				    <p>Miles de opiniones</p>
+				    <p><span class="material-icons">remove_red_eye</span>Miles de opiniones</p>
 			    </div>
 
 			    <div className="tip">
-				    <p>Precios finales</p>
+				    <p><span class="material-icons">credit_card</span>Precios finales</p>
 			    </div>
 
 		    </div>
--- web/src/pages/Pagefilter.js
+++ web/src/pages/Pagefilter.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import PageLayout from '../components/layout/PageLayout'
 import ActivitySelect from '../components/activities/ActivitySelect/ActivitySelect'
+import { Link } from 'react-router-dom';
+import './pagefilter.css';
 
 function Pagefilter() {
   return (
    <PageLayout>
+   <div className='row menudestinos'>
+      <div class="col">
+        <Link to = {`/activities/filter?destiny=Roma`}><p className='menudestinosp'>Roma</p></Link>
+      </div>
+      <div class="col">
+        <Link to = {`/activities/filter?destiny=Bali`}><p className='menudestinosp'>Bali</p></Link>
+      </div>
+      <div class="col">
+        <Link to = {`/activities/filter?destiny=NuevaYork`}><p className='menudestinosp'>Nueva York</p></Link>
+      </div>
+      <div class="col">
+        <Link to = {`/activities/filter?destiny=Egipto`}><p className='menudestinosp'>Egipto</p></Link>
+      </div>
+      <div class="col">
+        <Link to = {`/activities/filter?destiny=Paris`}><p className='menudestinosp'>París</p></Link>
+      </div>
+      <div class="col">
+        <Link to = {`/activities/filter?destiny=Londres`}><p className='menudestinosp'>Londres</p></Link>
+      </div>
+    </div>
     <div className="margin-botom">
       <ActivitySelect/>
     </div>
